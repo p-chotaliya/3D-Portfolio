ef3539b04931ce8f6710724b2e8491643445be1a
p-chotaliya <chotaliyaparth.pc@gmail.com>
2024-11-11 18:02:56 -0500

    added github pages and update config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/3D-Portfolio/',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 5000, // Adjust the limit as needed
+  },
 })
